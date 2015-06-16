8189dff80276d9fc2bd0b8230c8daddc64f755a4
Aliz Hammond <randomdude@gmail.com>
2015-06-16 03:20:04 +0100

    Add "-c" argument to permit 'continuous mode'

--- apc.py
+++ apc.py
@@ -5,6 +5,16 @@ import time
 import re
 import sys
 
+def doUsage():
+	print ('apc.py: Read output current from AP7641 (maybe others?) via RS232.')
+	print ('Will print current use on the commandline and return zero, or -1 on error.')
+	print ('Uses 19200 baud - I could find no options to change this on the PDU, so it')
+	print ('is hardcoded appropriately in apc.py.')
+	print ('Supply -c to poll continuously, until killed.')
+	print ('Caution: this code is pretty brittle, don\'t rely on it unless you don\'t mind fixing it!')
+	print ('Usage: apc.py [serial port] <-c>')
+	print ('eg. apc.py /dev/ttyS0')
+
 # for python 3 must explicitly convert bytes to strings and vice-versa
 # and must tell ser.write etc that we're sending bytes. Print is also a
 # bit different
@@ -19,16 +29,18 @@ def getCurrentScreen(s):
 		if read == '' and dataRead != '':
 			return dataRead
 
-if len(sys.argv) != 2:
-	print ('apc.py: Read output current from AP7641 (maybe others?) via RS232.')
-	print ('Will print current use on the commandline and return zero, or -1 on error.')
-	print ('Uses 19200 baud - I could find no options to change this on the PDU, so it')
-	print ('is hardcoded appropriately in apc.py.')
-	print ('Caution: this code is pretty brittle, don\'t rely on it unless you don\'t mind fixing it!')
-	print ('Usage: apc.py [serial port]')
-	print ('eg. apc.py /dev/ttyS0')
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+	doUsage
 	sys.exit(-1)
 port = sys.argv[1]
+contin = False
+if (len(sys.argv) > 2):
+	if (sys.argv[2] != '-c'):
+		doUsage()
+		sys.exit(-1)
+	contin = True
+
+print contin
 
 ser = serial.Serial(port=port, baudrate=19200, timeout=0.1)
 
@@ -44,8 +56,7 @@ while i < 5:
 # Open the 'status' menu. It is number 1. 
 ser.write(b'1\r\n')
 time.sleep(0.1)
-# I'm running this from the command line so I just want it to update the displayed
-# line continuously until I Ctrl-C my way out
+
 while True:
 	# Grab the current display, split it into lines
 	screen = getCurrentScreen(ser);
@@ -58,7 +69,10 @@ while True:
 	for line in lines:
 		m = re.match('.*\[K\\x1b\[6\;30H([0-9\.]+).*', line)
 		if m != None:
-			print (m.group(1), end='\r')
+			print ("%s\n", m.group(1) )
+
+	if contin == False:
+		break;
 
 	time.sleep(5)
 
